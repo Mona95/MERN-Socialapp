9348b296d0c01d2e51bd8da1552d8c67586c1ae8
Mona95 <shamona9594@gmail.com>
2021-12-26 16:45:57 +0300

    feat: first attempt to use context API

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,16 @@
+import { useRef } from "react";
 // Styles
 import "./login.scss";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
+  };
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -12,13 +21,28 @@ export default function Login() {
           </span>
         </div>
         <div className="login_wrapper__right">
-            <div className="box">
-                <input type="email" placeholder="Email" className="login-input"/>
-                <input type="password" placeholder="Password" className="login-input"/>
-                <button className="login-button">Log In</button>
-                <span className="login-forgot">Forgot Password?</span>
-                <button className="login-register-button">Create a New Account</button>
-            </div>
+          <form className="box" onSubmit={handleSubmit}>
+            <input
+              ref={email}
+              required
+              type="email"
+              placeholder="Email"
+              className="login-input"
+            />
+            <input
+              ref={password}
+              minLength="6"
+              required
+              type="password"
+              placeholder="Password"
+              className="login-input"
+            />
+            <button className="login-button">Log In</button>
+            <span className="login-forgot">Forgot Password?</span>
+            <button className="login-register-button">
+              Create a New Account
+            </button>
+          </form>
         </div>
       </div>
     </div>
